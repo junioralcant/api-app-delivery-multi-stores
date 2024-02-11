ac4fb50338b889aef96f4b42543f8a3e3b284ad0
junioralcant <junioralcant111@gmail.com>
2024-02-11 11:42:14 -0300

    feat: create request_http_adapter

---
+++ src/main/adapters/request_http_adapter.py
@@ -0,0 +1,19 @@
+from flask import Request as FlaskRequest
+from src.presentation.contracts.controller_contract import ControllerContract
+from src.presentation.http_type.http_request_types import HttpRequest
+from src.presentation.http_type.http_response_types import HttpResponse
+
+def request_http_adapter(request: FlaskRequest, controller: ControllerContract) -> HttpResponse:
+    body  = None
+    if request.data: body = request.json
+
+    http_request = HttpRequest(
+        body=body,
+        headers=request.headers,
+        query_params=request.args,
+        path_params=request.view_args,
+        url=request.url,
+    )
+
+    http_response = controller.handle(http_request)
+    return http_response
