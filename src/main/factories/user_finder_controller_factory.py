4145c9ff2dbb8b0d4055302aa3823e4cc0f08c2b
junioralcant <junioralcant111@gmail.com>
2024-02-11 12:16:57 -0300

    chore: config format autosave

--- src/main/factories/user_finder_controller_factory.py
+++ src/main/factories/user_finder_controller_factory.py
@@ -2,7 +2,8 @@ from src.data.usecases.user_finder_usecases import UserFinderUseCase
 from src.infra.repositories.users_repository import UsersRepository
 from src.presentation.controller.user_finder_controller import UserFinderController
 
+
 def user_finder_controller_factory() -> UserFinderController:
-    userRepository = UsersRepository
-    user_use_case = UserFinderUseCase(userRepository)
-    return UserFinderController(user_use_case) 
+    user_repository = UsersRepository
+    user_use_case = UserFinderUseCase(user_repository)
+    return UserFinderController(user_use_case)
